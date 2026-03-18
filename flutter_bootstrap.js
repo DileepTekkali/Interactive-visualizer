83313251861f00aae4d0aef590b130d20504d655
Tekkali Dileep kumar <dileepkumar66682@gmail.com>
2026-03-18 23:50:11 +0530

    Deploy v1.2 - New dashboard UI

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"e4b8dca3f1b4ede4c30371002441c88c12187e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1395156045" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1596794076" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
